21c8ef8dd7fe86217061b0c5c569086547ca5923
stormslowly <stormslowly@gmail.com>
2013-12-03 15:54:32 +0800

    finish forEach function

---
+++ forEach_study.js
@@ -0,0 +1,88 @@
+(function(){
+
+"use strict";
+
+
+var async = require("async");
+
+var tasks =[{name:"wsy",lazy:200},
+        {name:"wxx",lazy:300},
+        {name:"spf",lazy:100}];
+
+
+// all goes right
+async.forEach(tasks, 
+  function(task,cb){
+    console.log("1 do the task for ",task.name);
+    setTimeout(function(){
+      console.log("1 finish the taks for ", task.name);
+      cb(null,task.name);
+    }, task.lazy);
+
+  },
+  function(err,who){
+    console.log("1 error is ",err,"from ",who);
+  });
+/*
+do the task for  wsy
+do the task for  wxx
+do the task for  sfp
+finish the taks for  sfp
+finish the taks for  wsy
+finish the taks for  wxx
+error is  null from  undefined
+
+*/
+
+// one error in task, async callback called once for error
+async.forEach(tasks, 
+function(task,cb){
+  console.log("2 do the task for ",task.name);
+  setTimeout(function(){
+    console.log("2 finish the taks for ", task.name);
+    if(task.name === "spf")
+      cb("2 stupid happens",task.name);
+    else
+      cb(null,task.name);
+  }, task.lazy);
+
+},
+function(err,who){
+  console.log("2 error is ",err,"from ",who);
+});
+
+/*
+do the task for  wsy
+do the task for  wxx
+do the task for  spf
+finish the taks for  spf
+error is  stupid happens from  undefined
+finish the taks for  wsy
+finish the taks for  wxx
+*/           
+
+// more than 1 errors happen, callback called onece for the first error
+// tasks continues  
+async.forEach(tasks, 
+function(task,cb){
+  console.log("3 do the task for ",task.name);
+  setTimeout(function(){
+    console.log("3 finish the taks for ", task.name);
+    cb("3 stupid happens 4 "+task.name ,task.name);
+  }, task.lazy);
+
+},
+function(err,who){
+  console.log("3 error is ",err,"from ",who);
+});
+/*
+do the task for  wsy
+do the task for  wxx
+do the task for  spf
+finish the taks for  spf
+error is  stupid happens 4 spf from  undefined
+finish the taks for  wsy
+finish the taks for  wxx
+*/
+
+})();
